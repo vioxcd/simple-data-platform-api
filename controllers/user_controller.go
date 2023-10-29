6893dd5e0acd0568b356ed2da548b249146db288
RuslanUchan <ruslanuchan97@gmail.com>
2023-10-30 02:20:47 +0700

    feat: forbid non-admin to add new users

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -6,10 +6,44 @@ import (
 	"vioxcd/dpl/models"
 	"vioxcd/dpl/repo"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
+const (
+	ROLE_ADMIN = 1
+)
+
 func AddUser(c echo.Context) error {
+	token, ok := c.Get("user").(*jwt.Token)
+	if !ok {
+		return c.JSON(400, models.BaseResponse{
+			Status:  false,
+			Message: "JWT token missing or invalid",
+			Data:    nil,
+		})
+	}
+
+	// not sure why `claims := user.Claims.(*jwtCustomClaims)` doesn't work
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return c.JSON(500, models.BaseResponse{
+			Status:  false,
+			Message: "Failed to cast claims as jwt.MapClaims",
+			Data:    claims,
+		})
+	}
+
+	// https://stackoverflow.com/a/70706082
+	if uint(claims["roleId"].(float64)) != uint(ROLE_ADMIN) {
+		return c.JSON(403, models.BaseResponse{
+			Status:  false,
+			Message: "Unauthorized to create user",
+			Data:    nil,
+		})
+	}
+
+	// pass privilege checks, creating user
 	var user models.User
 	c.Bind(&user)
 
