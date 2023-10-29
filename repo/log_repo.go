898cf5421e16cbeb6c310584b0f453b5160c8d0a
RuslanUchan <ruslanuchan97@gmail.com>
2023-10-29 21:37:53 +0700

    tidy: remove print statements

--- repo/log_repo.go
+++ repo/log_repo.go
@@ -1,8 +1,6 @@
 package repo
 
 import (
-	"fmt"
-
 	"vioxcd/dpl/config"
 	"vioxcd/dpl/models"
 )
@@ -14,12 +12,10 @@ func TriggerNewRun(log *models.UserLog) error {
 
 func GetLog(log *models.UserLog) error {
 	result := config.DB.Joins("Run").Joins("User").Find(log)
-	fmt.Println(log)
 	return result.Error
 }
 
 func GetLogs(logs *[]models.UserLog) error {
 	result := config.DB.Joins("Run").Joins("User").Find(logs)
-	fmt.Println(logs)
 	return result.Error
 }
